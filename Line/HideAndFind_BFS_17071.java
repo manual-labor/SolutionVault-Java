0188c3c25da56d42df1cb94628b8f6820ae64045
Ohdonghyeon <fiveastring@gmail.com>
2025-04-19 05:29:04 +0900

    feat(line): 숨바꼭질5 임시저장

--- Line/HideAndFind_BFS_17071.java
+++ Line/HideAndFind_BFS_17071.java
@@ -1,15 +1,58 @@
+// https://www.acmicpc.net/problem/17071
+
 package Line;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import java.util.*;
+import java.io.*;
 public class HideAndFind_BFS_17071 {
-    
 
+    static boolean isOverRange(int location) {
+        if(location >= 0 && location <= 500000)
+            return false;
+        else return true;
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = reader.readLine();
+        int subin = Integer.parseInt(input[0]);
+        int brother = Integer.parseInt(input[1]);
+
+        HashSet<Integer> alreadyReached = new HashSet<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+
         
+        queue.offer(subin);
+
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                int location = queue.poll();
+
+                if (location == brother) {
+                    System.out.println(time);
+                    break;
+                }
+                int forwardWalk = location + 1;
+                int backwardWalk = location - 1;
+                int warp = location * 2;
+                if (!isOverRange(forwardWalk))
+                    queue.offer(forwardWalk);
+
+                if (location > 0)
+                    queue.offer(backwardWalk);
+
+                if (!isOverRange(warp))
+                    queue.offer(warp);
+
+            }
+            time++;
+            brother+=time;
+
+        }
 
     }
 }
